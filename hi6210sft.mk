bcf49734229a25fb4e4956e784827050707a92e5
hak86 <hakamelassouad@gmail.com>
2016-03-10 14:05:50 +0100

    P8L: separate blobs, keylayout and ramdisk in proper files.

--- hi6210sft.mk
+++ hi6210sft.mk
@@ -1,6 +1,8 @@
 # Android Opeu Source System Base
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, build/target/product/full_base.mk)
+
 
 # AAPT Settings
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
@@ -22,16 +24,20 @@ PRODUCT_PACKAGES += \
 	charger_res_images
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/charger/battery_fail.png:root/res/images/charger/battery_fail.png \
-	$(LOCAL_PATH)/charger/battery_scale.png:root/res/images/charger/battery_scale.png
+	device/huawei/hi6210sft/charger/battery_fail.png:root/res/images/charger/battery_fail.png \
+	device/huawei/hi6210sft/charger/battery_scale.png:root/res/images/charger/battery_scale.png
 
 # Chromium 
 PRODUCT_COPY_FILES := \
-	$(LOCAL_PATH)/chromium/libwebviewchromium64.so:system/lib64/libwebviewchromium.so \
-	$(LOCAL_PATH)/chromium/libwebviewchromium.so:system/lib/libwebviewchromium.so
+	device/huawei/hi6210sft/chromium/libwebviewchromium64.so:system/lib64/libwebviewchromium.so \
+	device/huawei/hi6210sft/chromium/libwebviewchromium.so:system/lib/libwebviewchromium.so
 
-# Device Path
-LOCAL_PATH := device/huawei/hi6210sft
+
+# call dalvik heap config
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+
+# call hwui memory config
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 # File System
 PRODUCT_PACKAGES += \
@@ -90,53 +96,22 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += system/core/rootdir/init.zygote64_32.rc:root/init.zygote64_32.rc
 
 # Kernel, My Own Cannabis :D 
-# ifeq ($(TARGET_PREBUILT_KERNEL),)
-# 	LOCAL_KERNEL := device/huawei/hi6210sft/kernel
-# else
-# 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-# endif
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := device/huawei/hi6210sft/kernel
+else
+ 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
 
-#PRODUCT_COPY_FILES += \
-#    $(LOCAL_KERNEL):kernel
-
-# keylayouts
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
-	$(LOCAL_PATH)/keylayout/Generic.kl:system/usr/keylayout/Generic.kl \
-	$(LOCAL_PATH)/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_0b05_Product_4500.kl:system/usr/keylayout/Vendor_0b05_Product_4500.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_1bad_Product_f016.kl:system/usr/keylayout/Vendor_1bad_Product_f016.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_1bad_Product_f023.kl:system/usr/keylayout/Vendor_1bad_Product_f023.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_1bad_Product_f027.kl:system/usr/keylayout/Vendor_1bad_Product_f027.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_1bad_Product_f036.kl:system/usr/keylayout/Vendor_1bad_Product_f036.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_1d79_Product_0009.kl:system/usr/keylayout/Vendor_1d79_Product_0009.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_05ac_Product_0239.kl:system/usr/keylayout/Vendor_05ac_Product_0239.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_12bd_Product_d015.kl:system/usr/keylayout/Vendor_12bd_Product_d015.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_18d1_Product_2c40.kl:system/usr/keylayout/Vendor_18d1_Product_2c40.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_22b8_Product_093d.kl:system/usr/keylayout/Vendor_22b8_Product_093d.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_028e.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_046d_Product_c21d.kl:system/usr/keylayout/Vendor_046d_Product_c21d.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_046d_Product_c21f.kl:system/usr/keylayout/Vendor_046d_Product_c21f.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_046d_Product_c216.kl:system/usr/keylayout/Vendor_046d_Product_c216.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_046d_Product_c219.kl:system/usr/keylayout/Vendor_046d_Product_c219.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_046d_Product_c294.kl:system/usr/keylayout/Vendor_046d_Product_c294.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_046d_Product_c299.kl:system/usr/keylayout/Vendor_046d_Product_c299.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_046d_Product_c532.kl:system/usr/keylayout/Vendor_046d_Product_c532.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_054c_Product_0268.kl:system/usr/keylayout/Vendor_054c_Product_0268.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_0079_Product_0011.kl:system/usr/keylayout/Vendor_0079_Product_0011.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_0583_Product_2060.kl:system/usr/keylayout/Vendor_0583_Product_2060.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_1038_Product_1412.kl:system/usr/keylayout/Vendor_1038_Product_1412.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_1689_Product_fd00.kl:system/usr/keylayout/Vendor_1689_Product_fd00.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_1689_Product_fd01.kl:system/usr/keylayout/Vendor_1689_Product_fd01.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_1689_Product_fe00.kl:system/usr/keylayout/Vendor_1689_Product_fe00.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_1949_Product_0401.kl:system/usr/keylayout/Vendor_1949_Product_0401.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_2378_Product_100a.kl:system/usr/keylayout/Vendor_2378_Product_100a.kl \
-	$(LOCAL_PATH)/keylayout/Vendor_2378_Product_1008.kl:system/usr/keylayout/Vendor_2378_Product_1008.kl 
+    $(LOCAL_KERNEL):kernel
+
+# Keys
+$(call inherit-product-if-exists, device/huawei/hi6210sft/keylayout.mk)
 
 # Media
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
-	$(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml
+	device/huawei/hi6210sft/media/media_codecs.xml:system/etc/media_codecs.xml \
+	device/huawei/hi6210sft/media/media_profiles.xml:system/etc/media_profiles.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
@@ -150,24 +125,23 @@ PRODUCT_PACKAGES += \
     	libnfc_nci_jni \
     	nfc_nci.bcm2079x.default \
     	NfcNci \
-    	Tag 
-    
+    	Tag     
 
 # NFCEE access control
-NFCEE_ACCESS_PATH := $(LOCAL_PATH)/nfc/nfcee_access.xml
+NFCEE_ACCESS_PATH := device/huawei/hi6210sft/nfc/nfcee_access.xml
 
 # NFC firmware
 PRODUCT_COPY_FILES += \
     	$(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml \
-	$(LOCAL_PATH)/nfc/lib/libnfc-nci.so:system/lib/libnfc-nci.so \
-	$(LOCAL_PATH)/nfc/lib/libnfc_nci_jni.so:system/lib/libnfc_nci_jni.so \
-	$(LOCAL_PATH)/nfc/lib/libnfc_ndef.so:system/lib64/libnfc_ndef.so \
-	$(LOCAL_PATH)/nfc/lib64/libnfc-nci.so:system/lib64/libnfc-nci.so \
-	$(LOCAL_PATH)/nfc/lib64/libnfc_nci_jni.so:system/lib64/libnfc_nci_jni.so \
-	$(LOCAL_PATH)/nfc/lib64/libnfc_ndef.so:system/lib64/libnfc_ndef.so \
-	$(LOCAL_PATH)/nfc/lib64/hw/nfc_nci.pn54x.default.so:system/lib64/hw/nfc_nci.pn54x.default.so \
-    	$(LOCAL_PATH)/nfc/libnfc-brcm-alice.conf:system/etc/libnfc-brcm-alice.conf \
-    	$(LOCAL_PATH)/nfc/libnfc-nxp-alice.conf:system/etc/libnfc-nxp-alice.conf 
+	device/huawei/hi6210sft/nfc/lib/libnfc-nci.so:system/lib/libnfc-nci.so \
+	device/huawei/hi6210sft/nfc/lib/libnfc_nci_jni.so:system/lib/libnfc_nci_jni.so \
+	device/huawei/hi6210sft/nfc/lib/libnfc_ndef.so:system/lib64/libnfc_ndef.so \
+	device/huawei/hi6210sft/nfc/lib64/libnfc-nci.so:system/lib64/libnfc-nci.so \
+	device/huawei/hi6210sft/nfc/lib64/libnfc_nci_jni.so:system/lib64/libnfc_nci_jni.so \
+	device/huawei/hi6210sft/nfc/lib64/libnfc_ndef.so:system/lib64/libnfc_ndef.so \
+	device/huawei/hi6210sft/nfc/lib64/hw/nfc_nci.pn54x.default.so:system/lib64/hw/nfc_nci.pn54x.default.so \
+    	device/huawei/hi6210sft/nfc/libnfc-brcm-alice.conf:system/etc/libnfc-brcm-alice.conf \
+    	device/huawei/hi6210sft/nfc/libnfc-nxp-alice.conf:system/etc/libnfc-nxp-alice.conf 
 
 # NFC permissions
 PRODUCT_COPY_FILES += \
@@ -175,73 +149,31 @@ PRODUCT_COPY_FILES += \
     	frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     	frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
 
+# Ramdisk
+$(call inherit-product-if-exists, device/huawei/hi6210sft/ramdisk.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/huawei/hi6210sft/overlay
 
-# Ramdisk
-PRODUC_COPY_FILES += \
-	$(LOCAL_PATH)/ramdisk/fstab.hi6210sft:root/fstab.hi6210sft \
-	$(LOCAL_PATH)/ramdisk/init:root/init \
-	$(LOCAL_PATH)/ramdisk/init.5801.rc:root/init.5801.rc \
-	$(LOCAL_PATH)/ramdisk/init.6165.rc:root/init.6165.rc \
-	$(LOCAL_PATH)/ramdisk/init.10106.rc:root/init.10106.rc \
-	$(LOCAL_PATH)/ramdisk/init.51054.rc:root/init.51054.rc \
-	$(LOCAL_PATH)/ramdisk/init.102173.rc:root/init.102173.rc \
-	$(LOCAL_PATH)/ramdisk/init.142782.rc:root/init.142782.rc \
-	$(LOCAL_PATH)/ramdisk/init.audio.rc:root/init.audio.rc \
-	$(LOCAL_PATH)/ramdisk/init.chip.usb.rc:root/init.chip.usb.rc \
-	$(LOCAL_PATH)/ramdisk/init.connectivity.bcm43xx.rc:root/init.connectivity.bcm43xx.rc \
-	$(LOCAL_PATH)/ramdisk/init.connectivity.hi110x.rc:root/init.connectivity.hi110x.rc \
-	$(LOCAL_PATH)/ramdisk/init.connectivity.rc:root/init.connectivity.rc \
-	$(LOCAL_PATH)/ramdisk/init.device.rc:root/init.device.rc \
-	$(LOCAL_PATH)/ramdisk/init.environ.rc:root/init.environ.rc \
-	$(LOCAL_PATH)/ramdisk/init.extmodem.rc:root/init.extmodem.rc \
-	$(LOCAL_PATH)/ramdisk/init.hi6210sft.rc:root/init.hi6210sft.rc \
-	$(LOCAL_PATH)/ramdisk/init.hisi.rc:root/init.hisi.rc \
-	$(LOCAL_PATH)/ramdisk/init.manufacture.rc:root/init.manufacture.rc \
-	$(LOCAL_PATH)/ramdisk/init.performance.rc:root/init.performance.rc \
-	$(LOCAL_PATH)/ramdisk/init.platform.rc:root/init.platform.rc \
-	$(LOCAL_PATH)/ramdisk/init.protocol.rc:root/init.protocol.rc \
-	$(LOCAL_PATH)/ramdisk/init.rc:root/init.rc \
-	$(LOCAL_PATH)/ramdisk/init.recovery.hi110x.rc:root/init.recovery.hi110x.rc \
-	$(LOCAL_PATH)/ramdisk/init.recovery.hi6210sft.rc:root/init.recovery.hi6210sft.rc \
-	$(LOCAL_PATH)/ramdisk/init.tee.rc:root/init.tee.rc \
-	$(LOCAL_PATH)/ramdisk/init.trace.rc:root/init.trace.rc \
-	$(LOCAL_PATH)/ramdisk/init.usb.rc:root/init.usb.rc \
-	$(LOCAL_PATH)/ramdisk/init.zygote32.rc:root/init.zygote32.rc \
-	$(LOCAL_PATH)/ramdisk/init.zygote64_32.rc:root/init.zygote64_32.rc \
-	$(LOCAL_PATH)/ramdisk/ueventd.5801.rc:root/ueventd.5801.rc \
-	$(LOCAL_PATH)/ramdisk/ueventd.6165.rc:root/ueventd.6165.rc \
-	$(LOCAL_PATH)/ramdisk/ueventd.10106.rc:root/ueventd.10106.rc \
-	$(LOCAL_PATH)/ramdisk/ueventd.51054.rc:root/ueventd.51054.rc \
-	$(LOCAL_PATH)/ramdisk/ueventd.102173.rc:root/ueventd.102173.rc \
-	$(LOCAL_PATH)/ramdisk/ueventd.142782.rc:root/ueventd.142782.rc \
-	$(LOCAL_PATH)/ramdisk/ueventd.hi6210sft.rc:root/ueventd.hi6210sft.rc \
-	$(LOCAL_PATH)/ramdisk/ueventd.rc:root/ueventd.rc
-
-# Sepolicy Configuration
+# Sepolicy
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/sepolicy/file_contexts:root/file_contexts \
-	$(LOCAL_PATH)/sepolicy/property_contexts:root/property_contexts \
-	$(LOCAL_PATH)/sepolicy/seapp_contexts:root/seapp_contexts \
-	$(LOCAL_PATH)/sepolicy/service_contexts:root/service_contexts 
+	device/huawei/hi6210sft/sepolicy/file_contexts:root/file_contexts \
+	device/huawei/hi6210sft/sepolicy/property_contexts:root/property_contexts \
+	device/huawei/hi6210sft/sepolicy/seapp_contexts:root/seapp_contexts \
+	device/huawei/hi6210sft/sepolicy/service_contexts:root/service_contexts 
 
 # Thermal
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/thermald.xml:system/etc/thermald.xml \
-	$(LOCAL_PATH)/configs/thermald_performance.xml:system/etc/thermald_performance.xml
+	device/huawei/hi6210sft/configs/thermald.xml:system/etc/thermald.xml \
+	device/huawei/hi6210sft/configs/thermald_performance.xml:system/etc/thermald_performance.xml
 
 # Wifi
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/wpa/hostapd_hisi.conf:system/etc/wifi/hostapd_hisi.conf \
-	$(LOCAL_PATH)/wpa/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
-	$(LOCAL_PATH)/wpa/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
-	$(LOCAL_PATH)/wpa/wpa_supplicant_hisi.conf:system/etc/wifi/wpa_supplicant_hisi.conf \
-	$(LOCAL_PATH)/wpa/fw_bcm4343s_apsta_hw.bin:system/vendor/firmware/fw_bcm4343s_apsta_hw.bin \
-	$(LOCAL_PATH)/wpa/fw_bcm4343s_p2p_hw.bin:system/vendor/firmware/fw_bcm4343s_p2p_hw.bin \
-	$(LOCAL_PATH)/wpa/fw_bcm4343s_test_hw_apsta.bin:system/vendor/firmware/fw_bcm4343s_test_hw_apsta.bin \
-	$(LOCAL_PATH)/wpa/nvram4343s_hw.txt:system/vendor/firmware/nvram4343s_hw.txt \
-
+	device/huawei/hi6210sft/wpa/hostapd_hisi.conf:system/etc/wifi/hostapd_hisi.conf \
+	device/huawei/hi6210sft/wpa/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
+	device/huawei/hi6210sft/wpa/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+	device/huawei/hi6210sft/wpa/wpa_supplicant_hisi.conf:system/etc/wifi/wpa_supplicant_hisi.conf 
+	
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := cm_hi6210sft
