479fed0393bcd864c413afc771b503ff75f9fef3
hak86 <hakamelassouad@gmail.com>
2016-03-13 11:22:28 +0100

    hi6210sft: update board to support 64 bit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,36 +3,80 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/huawei/p8litekirin/BoardConfigVendor.mk
 
+# 64 Bit
+ANDROID_64=true
+ANDROID_ENABLE_RENDERSCRIPT := true
+TARGET_SUPPORTS_32_BIT_APPS := true
+TARGET_SUPPORTS_64_BIT_APPS := true
+TARGET_USES_64_BIT_BINDER := true
+TARGET_USES_HISI_DTIMAGE := true
+
 # Architecture
+ifneq ($(P8LITEKIRIN_32_BIT),true)
+TARGET_BOARD_SUFFIX := _64
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
-TARGET_CPU_SMP := true
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+TARGET_USES_64_BIT_BINDER := true
+else
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := cortex-a53
+endif
+
+TARGET_CPU_CORTEX_A53 := true
+
 
 # Board
 TARGET_NO_BOOTLOADER := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_BOARD_PLATFORM := kirin930
 TARGET_BOOTLOADER_BOARD_NAME := p8litekirin
+USE_OPENGL_RENDERER := true
+
+# Boot animation
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+
+# Hardware
+TARGET_HARDWARE_3D := true
+
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := hisi_dma_print=0 vmalloc=384M maxcpus=8 no_irq_affinity androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x07478000
+BOARD_KERNEL_CMDLINE := hisi_dma_print=0 vmalloc=384M maxcpus=8 no_irq_affinity androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x07b88000 --tags_offset 0x02988000
+TARGET_KERNEL_SOURCE := kernel/huawei/hi6210sft
+ifneq ($(P8LITEKIRIN_32_BIT),true)
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_USES_UNCOMPRESSED_KERNEL := true
+endif
+ifneq ($(FORCE_32_BIT),true)
+TARGET_KERNEL_CONFIG := cyanogenmod_hi6210sft_64_defconfig
+else
+TARGET_KERNEL_CONFIG := cyanogenmod_hi6210sft_defconfig
+endif
 
-TARGET_PREBUILT_KERNEL := device/huawei/p8litekirin/kernel
-# Use this for build from source (need declares)
-# TARGET_KERNEL_SOURCE := kernel/huawei
-# TARGET_KERNEL_CONFIG := aosp_p8litekirin_defconfig
+# Logging
+TARGET_USES_LOGD := false
+
 
 # Partitionsizes
 BOARD_BOOTIMAGE_PARTITION_SIZE := 25165824
@@ -47,3 +91,21 @@ RECOVERY_GRAPHICS_USE_LINELENGTH := true
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_HAS_LARGE_FILESYSTEM := true
+
+# SkTextBox for libtvout
+BOARD_USES_SKTEXTBOX := true
+
+
+# Wifi
+BOARD_WLAN_DEVICE                := bcmdhd
+BOARD_WLAN_DEVICE_REV            := bcm4343
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA          := "/system/vendor/firmware/fw_bcm4343s_hw.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/system/vendor/firmware/fw_bcm4343s_apsta_hw.bin"
+WIFI_DRIVER_FW_PATH_P2P          := "/system/vendor/firmware/fw_bcm4343s_p2p_hw.bin"
+WIFI_BAND                        := 802_11_ABG
