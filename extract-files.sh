738518ea5b3ecfabb409eb5aff3bb86c609b062c
hak86 <hakamelassouad@gmail.com>
2016-03-17 18:20:55 +0100

    hi6210sft: fix typo extractor

--- extract-files.sh
+++ extract-files.sh
@@ -1,17 +1,17 @@
-#!binsh
+#!/bin/sh
 
 VENDOR=huawei
 DEVICE=hi6210sft
 
-BASE=......vendor$VENDOR$DEVICEproprietary
+BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
 
-echo Pulling device files...
-for FILE in `cat proprietary-files.txt  grep -v ^#  grep -v ^$`; do
+echo "Pulling device files..."
+for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
     DIR=`dirname $FILE`
-    if [ ! -d $BASE$DIR ]; then
-        mkdir -p $BASE$DIR
+    if [ ! -d $BASE/$DIR ]; then
+        mkdir -p $BASE/$DIR
     fi
-    adb pull system$FILE $BASE$FILE
+    adb pull /system/$FILE $BASE/$FILE
 done
 
-.setup-makefiles.sh
+./setup-makefiles.sh
