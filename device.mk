b208024d515057bbc622e8ae8afad84f7dd86f04
Hakam El Assouad <hakamelassouad@gmail.com>
2016-03-25 11:44:16 +0100

    hi6210sft: ramdisk reduced

--- device.mk
+++ device.mk
@@ -11,6 +11,19 @@ endif
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
+	
+# Ramdisk
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/ramdisk/fstab.hi6210sft:root/fstab.hi6210sft \
+	$(LOCAL_PATH)/ramdisk/init:root/init \
+	$(LOCAL_PATH)/ramdisk/init.rc:root/init.rc \
+	$(LOCAL_PATH)/ramdisk/init.audio.rc:root/init.audio.rc \
+	$(LOCAL_PATH)/ramdisk/init.connectivity.rc:root/init.connectivity.rc \
+	$(LOCAL_PATH)/ramdisk/init.hi6210sft.rc:root/init.hi6210sft.rc \
+	$(LOCAL_PATH)/ramdisk/init.platform.rc:root/init.platform.rc \
+	$(LOCAL_PATH)/ramdisk/init.recovery.hi6210sft.rc:root/init.recovery.hi6210sft.rc \
+	$(LOCAL_PATH)/ramdisk/init.usb.rc:root/init.usb.rc \
+	$(LOCAL_PATH)/ramdisk/ueventd.rc:root/ueventd.rc
 
 	
 # Zygote	
@@ -22,4 +35,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_COPY_FILES += system/core/rootdir/init.zygote64_32.rc:root/init.zygote64_32.rc
 
-PRODUCT_PACKAGES += libGLES_android	
+PRODUCT_PACKAGES += libGLES_android
