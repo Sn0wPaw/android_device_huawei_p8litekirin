124b27abd8062c0f0f7490091098f632e83e5295
hak86 <hakamelassouad@gmail.com>
2016-04-27 12:10:54 +0200

    hi6210sft: remove broken ramdisk

--- hi6210sft.mk
+++ hi6210sft.mk
@@ -156,16 +156,8 @@ PRODUCT_COPY_FILES += \
 # Ramdisk
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/ramdisk/fstab.hi6210sft:root/fstab.hi6210sft \
-	$(LOCAL_PATH)/ramdisk/init.hi6210sft.rc:root/init.hi6210sft.rc \
 	$(LOCAL_PATH)/ramdisk/init.recovery.hi6210sft.rc:root/init.recovery.hi6210sft.rc \
-	$(LOCAL_PATH)/ramdisk/init.usb.hi6210sft.rc:root/init.usb.hi6210sft.rc \
-	$(LOCAL_PATH)/ramdisk/ueventd.hi6210sft.rc:root/ueventd.hi6210sft.rc
 
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/ramdisk/sbin/check_root:root/sbin/check_root \
-	$(LOCAL_PATH)/ramdisk/sbin/e2fsck_s:root/sbin/e2fsck_s \
-	$(LOCAL_PATH)/ramdisk/sbin/oeminfo_nvm_server:root/sbin/oeminfo_nvm_server \
-	$(LOCAL_PATH)/ramdisk/sbin/teecd:root/sbin/teecd
 
 # Zygote
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.zygote=zygote64_32
