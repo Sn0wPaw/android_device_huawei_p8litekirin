5663cd8c7c533d054a8b7dab758db64b2c2e7c67
hak86 <hakamelassouad@gmail.com>
2016-04-27 11:32:13 +0200

    hi6210sft: add core_64_bit to support 64 Bit

--- hi6210sft.mk
+++ hi6210sft.mk
@@ -1,6 +1,7 @@
 # Android Open Source Project Common System
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
 
 # ADB
 PRODUCT_PROPERTY_OVERRIDES += \
