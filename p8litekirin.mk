dd556f5bc6c4fe2c8469c4837de5d1730d7236cd
hak86 <hakamelassouad@gmail.com>
2016-05-31 22:54:00 +0200

    hi6210sft: add device features

--- p8litekirin.mk
+++ p8litekirin.mk
@@ -2,6 +2,11 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
+# Audio
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/system/etc/audio_effects.conf:system/etc/audio_effects.conf \
+        $(LOCAL_PATH)/system/etc/audio_policy.conf:system/etc/audio_policy.conf
+
 # Binaries
 PRODUCT_COPY_FILES += \
         $(LOCAL_PATH)/system/bin/bcm47531/glgps:system/bin/bcm47531/glgps \
@@ -39,6 +44,44 @@ PRODUCT_COPY_FILES += \
         $(LOCAL_PATH)/system/bin/thermal-daemon:system/bin/thermal-daemon \
         $(LOCAL_PATH)/system/bin/wpa_supplicant_hisi:system/bin/wpa_supplicant_hisi \
         $(LOCAL_PATH)/system/xbin/watchlssd:system/xbin/watchlssd
+
+# Camera
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/system/etc/camera_orientation.cfg:system/etc/camera_orientation.cfg \
+        $(LOCAL_PATH)/system/etc/camera_resolutions.cfg:system/etc/camera_resolutions.cfg \
+        $(LOCAL_PATH)/system/etc/camera_videosnapshot.cfg:system/etc/camera_videosnapshot.cfg
+
+# Codecs
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/system/etc/hisi_omx.cfg:system/etc/hisi_omx.cfg \
+        $(LOCAL_PATH)/system/etc/media_codecs.xml:system/etc/media_codecs.xml \
+        $(LOCAL_PATH)/system/etc/media_profiles.xml:system/etc/media_profiles.xml \
+
+# Chrome 32 Bit
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/system/lib/libwebviewchromium.so:system/lib/libwebviewchromium.so \
+        $(LOCAL_PATH)/system/lib/libwebviewchromium_loader.so:system/lib/libwebviewchromium_loader.so \
+        $(LOCAL_PATH)/system/lib/libwebviewchromium_plat_support.so:system/lib/libwebviewchromium_plat_support.so \
+
+# Chrome 64 Bit
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/system/lib64/libwebviewchromium.so:system/lib64/libwebviewchromium.so \
+        $(LOCAL_PATH)/system/lib64/libwebviewchromium_loader.so:system/lib64/libwebviewchromium_loader.so \
+        $(LOCAL_PATH)/system/lib64/libwebviewchromium_plat_support.so:system/lib64/libwebviewchromium_plat_support.so
+
+# Device Monitor
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/system/etc/device_state_monitor.conf:system/etc/device_state_monitor.conf
+
+# GPS
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/system/etc/goldeneye_config.xml:system/etc/goldeneye_config.xml \
+        $(LOCAL_PATH)/system/etc/gps.conf:system/etc/gps.conf \
+        $(LOCAL_PATH)/system/etc/gpsconfig.xml:system/etc/gpsconfig.xml \
+        $(LOCAL_PATH)/system/etc/hisi_cfg.ini:system/etc/hisi_cfg.ini \
+        $(LOCAL_PATH)/system/etc/hisi_cfg_alice.ini:system/etc/hisi_cfg_alice.ini \
+        $(LOCAL_PATH)/system/etc/hisi_cfg_cherry.ini:system/etc/hisi_cfg_cherry.ini \
+
 
 # Init
 PRODUCT_COPY_FILES += \
@@ -100,3 +143,16 @@ PRODUCT_COPY_FILES += \
         $(LOCAL_PATH)/sbin/check_root:root/sbin/check_root \
         $(LOCAL_PATH)/sbin/logctl_service:root/sbin/logctl_service \
         $(LOCAL_PATH)/sbin/oeminfo_nvm_server:root/sbin/oeminfo_nvm_server
+
+# Thermald
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/system/etc/thermald.xml
+        $(LOCAL_PATH)/system/etc/thermald_performance.xml
+
+# USB
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    	persist.sys.usb.config=mtp
+
+# USB OTG
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.sys.isUsbOtgEnabled=true
